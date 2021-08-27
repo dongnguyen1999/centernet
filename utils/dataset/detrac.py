31b96f05170218560c90525f5bba5b5c44fb3e44
Nguyễn Đông <nvdkg1999@gmail.com>
2021-08-27 19:33:59 +0700

    Update detrac.py

--- utils/dataset/detrac.py
+++ utils/dataset/detrac.py
@@ -187,6 +187,7 @@ class DataGenerator(Sequence):
             img = cv2.imread(os.path.join(self.train_path, filename))
 
             with open(os.path.join(self.mask_path, f'{subfolder}_mask.npy'), 'rb') as f:
+                mask = np.load(f)
                 mask = mask.reshape((mask.shape[0], mask.shape[1], 1))
                 img = np.multiply(img, mask)
 
@@ -224,6 +225,7 @@ class DataGenerator(Sequence):
 
             with open(os.path.join(self.mask_path, f'{subfolder}_mask.npy'), 'rb') as f:
                 mask = np.load(f)
+                mask = mask.reshape((mask.shape[0], mask.shape[1], 1))
                 img = np.multiply(img, mask)
 
             im_h, im_w = img.shape[:2]
