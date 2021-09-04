b217b8cd2e80da31b174b0d9a279c0274114d77b
Nguyễn Đông <nvdkg1999@gmail.com>
2021-09-04 20:10:17 +0700

    Update vn_vehicle.py

--- keras_centernet/dataset/vn_vehicle.py
+++ keras_centernet/dataset/vn_vehicle.py
@@ -22,6 +22,7 @@ def load_data(config: Config):
     test_df = test_dfs[0] if len(test_dfs) == 1 else pd.concat(test_dfs)
 
     le = LabelEncoder()
+    train_df.label = train_df['label'].apply(str)
     train_df = train_df[train_df.label != 'person']
     train_df.label = le.fit_transform(train_df.label)
     test_df.label = le.transform(test_df.label)
