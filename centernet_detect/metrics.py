5ad71c530b78f275e4824f000f790ca6d3a8e62d
Nguyễn Đông <nvdkg1999@gmail.com>
2021-09-17 19:01:41 +0700

    add map per class

--- centernet_detect/metrics.py
+++ centernet_detect/metrics.py
@@ -11,111 +11,127 @@ import cv2
 import pandas as pd
 
 def calculate_iou(gt, pred) -> float:
-	"""Calculates the IoU.
-	"""
-	xt1, yt1, xt2, yt2, lt = gt
-	xp1, yp1, xp2, yp2, lp = pred
+  """Calculates the IoU.
+  """
+  xt1, yt1, xt2, yt2, lt = gt
+  xp1, yp1, xp2, yp2, lp = pred
 
-	if lt != lp: return 0.0
+  if lt != lp: return 0.0
 
-	overlap_area = 0.0
-	union_area = 0.0
+  overlap_area = 0.0
+  union_area = 0.0
 
-	# Calculate overlap area
-	dx = min(xt2, xp2) - max(xt1, xp1)
-	dy = min(yt2, yp2) - max(yt1, yp1)
+  # Calculate overlap area
+  dx = min(xt2, xp2) - max(xt1, xp1)
+  dy = min(yt2, yp2) - max(yt1, yp1)
 
-	if (dx > 0) and (dy > 0):
-		overlap_area = dx * dy
+  if (dx > 0) and (dy > 0):
+    overlap_area = dx * dy
 
-	# Calculate union area
-	union_area = (
-		(xt2 - xt1) * (yt2 - yt1) +
-		(xp2 - xp1) * (yp2 - yp1) -
-		overlap_area
-	)
+  # Calculate union area
+  union_area = (
+    (xt2 - xt1) * (yt2 - yt1) +
+    (xp2 - xp1) * (yp2 - yp1) -
+    overlap_area
+  )
 
-	return overlap_area / union_area
+  return overlap_area / union_area
 
 def find_best_match(gts, predd, threshold=0.5):
-	"""Returns the index of the 'best match' between the
-	ground-truth boxes and the prediction. The 'best match'
-	is the highest IoU. (0.0 IoUs are ignored).
-	
-	Args:
-		gts: Coordinates of the available ground-truth boxes
-		pred: Coordinates of the predicted box
-		threshold: Threshold
-		form: Format of the coordinates
-		
-	Return:
-		Index of the best match GT box (-1 if no match above threshold)
-	"""
-	best_match_iou = -np.inf
-	best_match_idx = -1
-	
-	for gt_idx, ggt in enumerate(gts):
-		iou = calculate_iou(ggt, predd)
-		
-		if iou < threshold:
-			continue
-		
-		if iou > best_match_iou:
-			best_match_iou = iou
-			best_match_idx = gt_idx
+  """Returns the index of the 'best match' between the
+  ground-truth boxes and the prediction. The 'best match'
+  is the highest IoU. (0.0 IoUs are ignored).
+  
+  Args:
+    gts: Coordinates of the available ground-truth boxes
+    pred: Coordinates of the predicted box
+    threshold: Threshold
+    form: Format of the coordinates
+    
+  Return:
+    Index of the best match GT box (-1 if no match above threshold)
+  """
+  best_match_iou = -np.inf
+  best_match_idx = -1
+  
+  for gt_idx, ggt in enumerate(gts):
+    iou = calculate_iou(ggt, predd)
+    
+    if iou < threshold:
+      continue
+    
+    if iou > best_match_iou:
+      best_match_iou = iou
+      best_match_idx = gt_idx
 
-	return best_match_idx
+  return best_match_idx
 
-def calculate_precision(preds_sorted, gt_boxes, threshold=0.5):
-	"""Calculates precision per at one threshold.
-	
-	Args:
-		preds_sorted: 
-	"""
-	tp = 0
-	fp = 0
-	fn = 0
+def calculate_precision(preds_sorted, gt_boxes, num_classes, threshold=0.5):
+  """Calculates precision per at one threshold.
+  
+  Args:
+    preds_sorted: 
+  """
+  tp = 0
+  fp = 0
+  fn = 0
 
-	fp_boxes = []
+  cl_tp, cl_fn, cl_fp = np.array([0 for _ in range(num_classes)]), np.array([0 for _ in range(num_classes)]), np.array([0 for _ in range(num_classes)])
 
-	for pred_idx, pred in enumerate(preds_sorted):
-		best_match_gt_idx = find_best_match(gt_boxes, pred, threshold=threshold)
+  fp_boxes = []
 
-		if best_match_gt_idx >= 0:
-			# True positive: The predicted box matches a gt box with an IoU above the threshold.
-			tp += 1
+  for pred_idx, pred in enumerate(preds_sorted):
+    best_match_gt_idx = find_best_match(gt_boxes, pred, threshold=threshold)
 
-			# Remove the matched GT box
-			gt_boxes = np.delete(gt_boxes, best_match_gt_idx, axis=0)
+    if best_match_gt_idx >= 0:
+      # True positive: The predicted box matches a gt box with an IoU above the threshold.
+      tp += 1
+      _, _, _, _, label = gt_boxes[best_match_gt_idx]
+      cl_tp[label] += 1
+      # Remove the matched GT box
+      gt_boxes = np.delete(gt_boxes, best_match_gt_idx, axis=0)
 
-		else:
-			# No match
-			# False positive: indicates a predicted box had no associated gt box.
-			fp += 1
-			fp_boxes.append(pred)
+    else:
+      # No match
+      # False positive: indicates a predicted box had no associated gt box.
+      fp += 1
+      _, _, _, _, label = pred
+      cl_fp[label] += 1
+      fp_boxes.append(pred)
 
-	# False negative: indicates a gt box had no associated predicted box.
-	fn = len(gt_boxes)
-	precision = tp / (tp + fp + fn)
-	return precision, fp_boxes, gt_boxes
+  # False negative: indicates a gt box had no associated predicted box.
+  fn = len(gt_boxes)
+  for gtb in gt_boxes:
+    _, _, _, _, label = gtb
+    cl_fn[label] += 1
 
-def calculate_image_precision(preds_sorted, gt_boxes, thresholds=(0.5), debug=False):
-	
-	n_threshold = len(thresholds)
-	image_precision = 0.0
-	threshold_precision = []
-	
-	for threshold in thresholds:
-		precision_at_threshold, _, _ = calculate_precision(preds_sorted,
-														   gt_boxes,
-														   threshold=threshold
-														)
-		if debug:
-			print("@{0:.2f} = {1:.4f}".format(threshold, precision_at_threshold))
-		threshold_precision.append(precision_at_threshold)
-		image_precision += precision_at_threshold / n_threshold
-	
-	return image_precision, np.array(threshold_precision)
+  precision = tp / (tp + fp + fn + 0.0000001)
+  cl_precision = cl_tp / ((cl_tp + cl_fp + cl_fn) + 0.0000001)
+  return precision, cl_precision, fp_boxes, gt_boxes
+
+def calculate_image_precision(preds_sorted, gt_boxes, num_classes, thresholds=(0.5), debug=False):
+  
+  n_threshold = len(thresholds)
+  image_precision = 0.0
+  threshold_precision = []
+
+  image_cl_precision = np.array([0.0 for _ in range(num_classes)])
+  threshold_cl_precision = []
+  
+  for threshold in thresholds:
+    precision_at_threshold, cl_precision_at_threshold, _, _ = calculate_precision(preds_sorted,
+                               gt_boxes, num_classes,
+                               threshold=threshold
+                            )
+    if debug:
+      print("@{0:.2f} = {1:.4f}".format(threshold, precision_at_threshold))
+
+    threshold_precision.append(precision_at_threshold)
+    threshold_cl_precision.append(cl_precision_at_threshold)
+    image_precision += precision_at_threshold / n_threshold
+    image_cl_precision += cl_precision_at_threshold / n_threshold
+  
+  return image_precision, np.array(threshold_precision), image_cl_precision, np.array(threshold_cl_precision)
 
 def calcmAP(model, valid_df, config: Config, confidence=0.5, thresholds=np.arange(0.5, 0.76, 0.05), path=None):
   model_ = CtDetDecode(model, config.num_classes)
@@ -123,7 +139,9 @@ def calcmAP(model, valid_df, config: Config, confidence=0.5, thresholds=np.arang
   iou_thresholds = [x for x in thresholds]
   
   precision = []
+  cl_precision = []
   threshold_precisions = np.array([0.0 for _ in iou_thresholds])
+  threshold_cl_precisions = np.zeros((len(iou_thresholds), config.num_classes))
 
   image_ids = valid_df[config.image_id].unique()
   countN = 0
@@ -164,17 +182,20 @@ def calcmAP(model, valid_df, config: Config, confidence=0.5, thresholds=np.arang
     preds_sorted = pred_box[preds_sorted_idx]
 
     if len(boxes) > 0:
-      image_precision, threshold_precision = calculate_image_precision(preds_sorted, boxes,
+      image_precision, threshold_precision, image_cl_precision, threshold_cl_precision = calculate_image_precision(preds_sorted, boxes, config.num_classes,
                                               thresholds=iou_thresholds, debug=False)
       precision.append(image_precision)
+      cl_precision.append(image_cl_precision)
       threshold_precisions += threshold_precision
+      threshold_cl_precisions += threshold_cl_precision
       countN += 1
     else:
       if len(preds_sorted) > 0:
         precision.append(0)
   
   precision = np.array(precision)
-  return np.mean(precision), (threshold_precisions / countN)
+  cl_precision = np.array(cl_precision)
+  return np.mean(precision), (threshold_precisions / countN), np.mean(cl_precision, axis=0), (threshold_cl_precisions / countN)
 
 class SaveBestmAP(Callback):
   def __init__(self, config: Config, path, valid_df, confidence=0.25, thresholds=np.arange(0.5, 0.76, 0.05)):
@@ -191,7 +212,7 @@ class SaveBestmAP(Callback):
     self.best_epoch = 0
 
   def on_epoch_end(self, epoch, logs=None):
-    current, maps = calcmAP(self.model, self.valid_df, self.config, confidence=self.confidence, thresholds=self.thresholds)
+    current, _, _, _ = calcmAP(self.model, self.valid_df, self.config, confidence=self.confidence, thresholds=self.thresholds)
     print('Current mAP: %.4f' % current)
     if np.greater(current, self.best):
       self.best = current
@@ -213,6 +234,7 @@ class TestmAP(Callback):
     self.test_df = test_df
     self.valid_df = valid_df
     self.test_paths = config.test_paths
+    self.num_classes = config.num_classes
     self.df = pd.DataFrame([])
 
   def on_epoch_end(self, epoch, logs=None):
@@ -221,13 +243,23 @@ class TestmAP(Callback):
     thresholds = [x for x in self.thresholds]
     
     print(f'Evalutate valid')
-    current, maps = calcmAP(self.model, self.valid_df, self.config, confidence=self.confidence, thresholds=self.thresholds)
+    current, maps, cl_maps, th_cl_maps = calcmAP(self.model, self.valid_df, self.config, confidence=self.confidence, thresholds=self.thresholds)
     r = [f'valid', epoch, current]
     names = ['test_id', 'epoch', 'mAP']
     for index, iou in enumerate(thresholds):
-      print('mAP@%.2f: %.4f' % (iou, maps[index]))
+      # print('mAP@%.2f: %.4f' % (iou, maps[index]))
       r.append(maps[index])
       names.append('mAP@%.2f' % iou)
+
+    for cl in range(self.num_classes):
+      print('mAP_class%d: %.4f' % (cl, cl_maps[cl]))
+      r.append(cl_maps[cl])
+      names.append('mAP_class%d' % cl)
+      for index, iou in enumerate(thresholds):
+        # print('mAP@%.2f_class%d: %.4f' % (iou, cl, th_cl_maps[index, cl]))
+        r.append(th_cl_maps[index, cl])
+        names.append('mAP@%.2f_class%d' % (iou, cl))
+      
     print('valid mAP: %.4f' % current)
     rdf.append(r)
     columns = names
@@ -236,11 +268,19 @@ class TestmAP(Callback):
       test_id = test_index + 1
       print(f'Evalutate test{test_id}')
       test_df = self.test_df[self.test_df['test_id'] == test_id]
-      current, maps = calcmAP(self.model, test_df, self.config, path=test_path, confidence=self.confidence, thresholds=self.thresholds)
+      current, maps, cl_maps, th_cl_maps = calcmAP(self.model, test_df, self.config, path=test_path, confidence=self.confidence, thresholds=self.thresholds)
       r = [f'test{test_id}', epoch, current]
       for index, iou in enumerate(thresholds):
-        print('mAP@%.2f: %.4f' % (iou, maps[index]))
+        # print('mAP@%.2f: %.4f' % (iou, maps[index]))
         r.append(maps[index])
+
+      for cl in range(self.num_classes):
+        print('mAP_class%d: %.4f' % (cl, cl_maps[cl]))
+        r.append(cl_maps[cl])
+        for index, iou in enumerate(thresholds):
+          # print('mAP@%.2f_class%d: %.4f' % (iou, cl, th_cl_maps[index, cl]))
+          r.append(th_cl_maps[index, cl])
+          
       print('test%s mAP: %.4f' % (test_id, current))
       rdf.append(r)
 
