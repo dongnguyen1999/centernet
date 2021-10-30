786e7be53666a525a16b687093ecd582dfea987d
Nguyễn Đông <nvdkg1999@gmail.com>
2021-10-30 10:58:39 +0700

    Update test_background_subtraction.py

--- test_background_subtraction.py
+++ test_background_subtraction.py
@@ -5,7 +5,7 @@ from utils.limit_queue import LimitQueue
 
 queue_size = 5
 frame_queue = LimitQueue(queue_size)
-bs_threshold = 30
+bs_threshold = 80
 median_blur_kernel = 7
 
 # cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\dnnew_crowd.autosave\dnnew_crowd.autosave.mp4')
@@ -18,6 +18,7 @@ _, first_frame = cap.read()
 # gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
 subtractor = cv2.createBackgroundSubtractorMOG2(varThreshold=bs_threshold, detectShadows=False)
+
 # subtractor = cv2.createBackgroundSubtractorKNN(history=50, dist2Threshold=1000)
 
 
@@ -28,13 +29,23 @@ while True:
 
     prev_frame = frame_queue.top()
     prev_frame = cv2.resize(prev_frame, (512, 512))
+
     frame = cv2.resize(frame, (512, 512))
+
+    contrast = 3
+    brightness = -255
+    frame = cv2.addWeighted( frame, contrast, frame, 0, brightness)
+
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    frame = cv2.GaussianBlur(frame, (5, 5), 3)
+    # frame = cv2.medianBlur(frame, 1)
 
     # prev_mask = subtractor.apply(prev_frame)
     mask = subtractor.apply(frame)
 
     # prev_mask = cv2.medianBlur(prev_mask, median_blur_kernel)
-    mask = cv2.medianBlur(mask, median_blur_kernel)
+    mask = cv2.medianBlur(mask, 1)
     
     # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     # frame = cv2.GaussianBlur(frame, (5, 5), 0)
@@ -46,16 +57,32 @@ while True:
     cv2.imshow("Frame", frame)
     # cv2.imshow("Difference", difference)
 
-    ret, thresh = cv2.threshold(mask, 150, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    # contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # draw contours on the original image
+
     image_copy = frame.copy()
-    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
-                
+    im_h, im_w = image_copy.shape[:2]
+    
+    (contours, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # for contour in contours:
+    #     (x, y, w, h) = cv2.boundingRect(contour)
+    #     if ((w*h) / (im_w*im_h)) > 0.01:
+    #         cv2.rectangle(image_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    
+    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=-1, lineType=cv2.LINE_AA)            
+
+
+    mask_copy = mask.copy()
+
+    cv2.drawContours(image=mask_copy, contours=contours, contourIdx=-1, color=(255, 255, 255), thickness=-1, lineType=cv2.LINE_AA)
+
 
     cv2.imshow("Mask", mask)
     cv2.imshow("Contours", image_copy)
+    cv2.imshow("Mask Contours", mask_copy)
 
 
     # print(np.sum(mask > 0) / (mask.shape[0]*mask.shape[1]))
