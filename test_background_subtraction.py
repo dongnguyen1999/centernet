b31129c4228da2e59f900ea3c4717ec01889b112
Nguyễn Đông <nvdkg1999@gmail.com>
2021-10-31 20:12:47 +0700

    Update test_background_subtraction.py

--- test_background_subtraction.py
+++ test_background_subtraction.py
@@ -3,49 +3,64 @@ import numpy as np
 
 from utils.limit_queue import LimitQueue
 
-queue_size = 5
+queue_size = 2
 frame_queue = LimitQueue(queue_size)
 bs_threshold = 80
 median_blur_kernel = 7
 
-# cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\dnnew_crowd.autosave\dnnew_crowd.autosave.mp4')
+cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\dnnew_crowd.autosave\dnnew_crowd.autosave.mp4')
 # cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\test3_crowd\test3_crowd.mp4')
-cap = cv2.VideoCapture(r"D:\dnnew_crowd.mp4")
+# cap = cv2.VideoCapture(r"D:\dnnew_crowd.mp4")
 
 _, first_frame = cap.read()
 # gray_frame = cv2.cvtColor(first_frame, cv2.COLOR_RGB2GRAY)
 
 # gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
 
-subtractor = cv2.createBackgroundSubtractorMOG2(varThreshold=bs_threshold, detectShadows=False)
+subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=bs_threshold, detectShadows=False)
 
 # subtractor = cv2.createBackgroundSubtractorKNN(history=50, dist2Threshold=1000)
 
+def preprocess_frame(frame):
+    contrast = 3
+    brightness = -255
+    frame = cv2.addWeighted( frame, contrast, frame, 0, brightness)
+    frame = cv2.GaussianBlur(frame, (5, 5), 3)
+    return frame
 
+def postprocess_frame(mask):
+    mask = cv2.medianBlur(mask, 1)
+    # (contours, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask_copy = mask.copy()
+    # cv2.drawContours(image=mask_copy, contours=contours, contourIdx=-1, color=(255, 255, 255), thickness=-1, lineType=cv2.LINE_AA)            
+    return mask_copy
 
 while True:
     _, frame = cap.read()
     frame_queue.enqueue(frame)
 
     prev_frame = frame_queue.top()
-    prev_frame = cv2.resize(prev_frame, (512, 512))
 
-    frame = cv2.resize(frame, (512, 512))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    sub = frame-prev_frame
+    th = 80
+    sub[sub > th] = 1
+    sub[sub <= th] = 0
+    cv2.imshow("Subtract", sub)
 
-    contrast = 3
-    brightness = -255
-    frame = cv2.addWeighted( frame, contrast, frame, 0, brightness)
-
-    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    frame = cv2.GaussianBlur(frame, (5, 5), 3)
+    
     # frame = cv2.medianBlur(frame, 1)
+    frame = preprocess_frame(frame)
+    prev_frame = preprocess_frame(prev_frame)
 
-    # prev_mask = subtractor.apply(prev_frame)
+    prev_mask = subtractor.apply(prev_frame)
     mask = subtractor.apply(frame)
 
     # prev_mask = cv2.medianBlur(prev_mask, median_blur_kernel)
-    mask = cv2.medianBlur(mask, 1)
+    mask = postprocess_frame(mask)
+    prev_mask = postprocess_frame(prev_mask)
+    
     
     # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     # frame = cv2.GaussianBlur(frame, (5, 5), 0)
@@ -81,13 +96,14 @@ while True:
 
 
     cv2.imshow("Mask", mask)
-    cv2.imshow("Contours", image_copy)
-    cv2.imshow("Mask Contours", mask_copy)
+    # cv2.imshow("Contours", image_copy)
+    # cv2.imshow("Mask Contours", mask_copy)
 
+    diff_mask = mask-prev_mask
 
-    # print(np.sum(mask > 0) / (mask.shape[0]*mask.shape[1]))
+    print(np.sum(diff_mask > 0) / np.sum(prev_mask > 0))
 
-    # cv2.imshow("Diff Mask", mask-prev_mask)
+    # cv2.imshow("Diff Mask", diff_mask)
 
     key = cv2.waitKey(30)
     if key == 27:
