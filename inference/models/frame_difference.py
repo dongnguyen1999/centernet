822e0fdd55a4bdf636ccb862ef4d419bd355f6e5
Nguyễn Đông <55080498+dongnguyen1999@users.noreply.github.com>
2021-11-10 10:08:38 +0700

    update test bgs

--- inference/models/frame_difference.py
+++ inference/models/frame_difference.py
@@ -1,9 +1,9 @@
-
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 from inference.utils import get_masked_img
+
 
 class FrameDiffEstimator:
     def __init__(self, long_term_history=162000, short_term_history=1, threshold=70, debug=False):
@@ -14,8 +14,10 @@ class FrameDiffEstimator:
         self.refresh()
 
     def refresh(self):
-        self.long_subtractor = cv2.createBackgroundSubtractorMOG2(history=self.long_term_history, varThreshold=self.threshold, detectShadows=False)
-        self.short_subtractor = cv2.createBackgroundSubtractorMOG2(history=self.short_term_history, varThreshold=self.threshold, detectShadows=False)
+        self.long_subtractor = cv2.createBackgroundSubtractorMOG2(history=self.long_term_history,
+                                                                  varThreshold=self.threshold, detectShadows=False)
+        self.short_subtractor = cv2.createBackgroundSubtractorMOG2(history=self.short_term_history,
+                                                                   varThreshold=self.threshold, detectShadows=False)
 
     def feed(self, frame, mask=None):
         if mask is not None:
@@ -27,7 +29,7 @@ class FrameDiffEstimator:
         # increase contrast in image
         contrast = 3
         brightness = -255
-        frame = cv2.addWeighted( frame, contrast, frame, 0, brightness)
+        frame = cv2.addWeighted(frame, contrast, frame, 0, brightness)
 
         # use gaussian blur to reduce noise
         frame = cv2.GaussianBlur(frame, (5, 5), 3)
@@ -48,7 +50,7 @@ class FrameDiffEstimator:
 
         mask = self.short_subtractor.apply(frame)
 
-        source_img[mask > 0] = (0, 255, 0)
+        source_img[mask > 0] = (255, 0, 0)
 
         if self.debug == True: cv2.imshow("frame_subtraction", source_img)
 
@@ -71,14 +73,16 @@ class FrameDiffEstimator:
         (contours, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if self.debug == True:
-            cv2.drawContours(image=source_img, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=-1, lineType=cv2.LINE_AA)
+            cv2.drawContours(image=source_img, contours=contours, contourIdx=-1, color=(0, 255, 255), thickness=-1,
+                             lineType=cv2.LINE_AA)
             cv2.imshow("foreground_detection", source_img)
 
-        cv2.drawContours(image=mask_copy, contours=contours, contourIdx=-1, color=(255, 255, 255), thickness=-1, lineType=cv2.LINE_AA)
+        cv2.drawContours(image=mask_copy, contours=contours, contourIdx=-1, color=(255, 255, 255), thickness=-1,
+                         lineType=cv2.LINE_AA)
 
         return mask_copy
 
-    def apply(self, frame, mask=None):
+    def apply(self, frame, mask=None, return_diff_mask=False, return_foreground_mask=False):
 
         if mask is not None:
             frame = get_masked_img(frame, mask)
@@ -86,7 +90,13 @@ class FrameDiffEstimator:
         diff_mask = self.frame_subtraction(frame)
         foreground_mask = self.foreground_estimation(frame)
 
-        return (np.sum(diff_mask > 0) * 100) / np.sum(foreground_mask > 0)
+        result = {'diff_rate': (np.sum(diff_mask > 0) * 100) / (np.sum(foreground_mask > 0) + 1e-6)}
+        if return_diff_mask:
+            result['diff_mask'] = diff_mask
+        if return_foreground_mask:
+            result['foreground_mask'] = foreground_mask
+
+        return result
 
     def apply_range(self, frames, mask=None):
         for frame in frames:
--- test_background_subtraction.py
+++ test_background_subtraction.py
@@ -1,95 +1,43 @@
 import cv2
 import numpy as np
 
-from utils.limit_queue import LimitQueue
+from inference.models.frame_difference import FrameDiffEstimator
 
-queue_size = 5
-frame_queue = LimitQueue(queue_size)
 
 # cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\dnnew_crowd.autosave\dnnew_crowd.autosave.mp4')
 # cap = cv2.VideoCapture(r'C:\Users\nvdkg\Documents\Camtasia\test3_crowd\test3_crowd.mp4')
 cap = cv2.VideoCapture(r"D:\dnnew_crowd.mp4")
 
-_, first_frame = cap.read()
-# gray_frame = cv2.cvtColor(first_frame, cv2.COLOR_RGB2GRAY)
-
-# gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-
-# subtractor = cv2.createBackgroundSubtractorKNN(history=50, dist2Threshold=1000)
-long_subtractor = cv2.createBackgroundSubtractorMOG2(history=162000, varThreshold=50, detectShadows=False)
-short_subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=50, detectShadows=False)
-
-def preprocess(frame):
-    contrast = 3
-    brightness = -255
-    frame = cv2.addWeighted( frame, contrast, frame, 0, brightness)
-    frame = cv2.GaussianBlur(frame, (5, 5), 3)
-    return frame
-
-def posprocess(mask):
-    mask = cv2.medianBlur(mask, 1)
-    return mask
-
-def frame_subtraction(frame):
-    frame = cv2.resize(frame, (512, 512))
-    source_img = frame.copy()
-
-    frame = cv2.cvtColor(source_img, cv2.COLOR_BGR2GRAY)
-
-    mask = short_subtractor.apply(frame)
-
-    source_img[mask > 128] = (0, 255, 0)
-
-    # cv2.imshow("frame_subtraction", source_img)
-
-    return mask
-
-def foreground_estimation(frame):
-
-    frame = cv2.resize(frame, (512, 512))
-    source_img = frame.copy()
-
-    frame = preprocess(frame)
-
-    mask = long_subtractor.apply(frame)
-
-    (contours, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    cv2.drawContours(image=source_img, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=-1, lineType=cv2.LINE_AA)
-
-    # cv2.imshow("foreground_detection", source_img)
-
-    mask_copy = mask.copy()
-
-    cv2.drawContours(image=mask_copy, contours=contours, contourIdx=-1, color=(255, 255, 255), thickness=-1, lineType=cv2.LINE_AA)
-
-    return mask_copy
+frame_diff_estimator = FrameDiffEstimator(debug=True)
 
 while True:
     _, frame = cap.read()
 
-    frame_queue.enqueue(frame)
-    prev_frame = frame_queue.top()
+    frame = cv2.resize(frame, (512, 512))
     
-    # cv2.imshow("Frame", frame)
+    cv2.imshow("Frame0", frame)
+    cv2.imshow("Frame0.", frame)
     
-    diff_mask = frame_subtraction(frame)
-    
-    current_foreground = foreground_estimation(frame)
+    frame_diff_estimator.apply(frame)
     
     # cv2.imshow("Contours", image_copy)
     # cv2.imshow("Mask Contours", mask_copy)
 
-    cv2.imshow("diff_mask", diff_mask)
-    cv2.imshow("current_foreground", current_foreground)
+    # cv2.imshow("diff_mask", diff_mask)
+    # cv2.imshow("current_foreground", current_foreground)
 
-    print(np.sum(diff_mask > 0) / np.sum(current_foreground > 0))
+    # print(np.sum(diff_mask > 0) / np.sum(current_foreground > 0))
 
     # cv2.imshow("Diff Mask", diff_mask)
 
     key = cv2.waitKey(30)
     if key == 27:
         break
+    elif key == 32:
+        key1 = 0
+        while key1 != 32:
+            key1 = cv2.waitKey()
+
 
 cap.release()
 cv2.destroyAllWindows()
